c03813d496fcbd2f511392247b5691e60f758e2a
Igor Tkach <itkach@aarddict.org>
2014-08-13 22:16:59 -0400

    Increase thread pool size

--- src/main/java/itkach/slobber/Slobber.java
+++ src/main/java/itkach/slobber/Slobber.java
@@ -266,7 +266,7 @@ public class Slobber implements Container {
     }
 
     public Server start(String addrStr, int port) throws IOException {
-        Server server = new ContainerServer(this);
+        Server server = new ContainerServer(this, 16);
         Connection connection = new SocketConnection(server);
         SocketAddress address = new InetSocketAddress(InetAddress.getByName(addrStr), 8013);
         connection.connect(address);
